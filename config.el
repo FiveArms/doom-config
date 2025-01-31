827239bc97cf68815085db4413c0a11f1e5373ed
FiveArms <git@fivearms.page>
2025-01-31 11:34:55 -0700

    add font choice ibm plex

--- config.el
+++ config.el
@@ -32,6 +32,9 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font                (font-spec :family "IBM Plex Mono")
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans")
+      doom-serif-font          (font-spec :family "IBM Plex Serif"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
